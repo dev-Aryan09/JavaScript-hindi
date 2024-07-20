ca0663e7d6bdf899f1bbbb71333c96841cb768e8
AryanSaini <09aryan.lucky@gmail.com>
2024-07-20 13:27:47 +0530

    add datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict" // treat all JS code as newer version
+
+alert( 3 + 3) // using node js, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+    console.log("Hitesh");
