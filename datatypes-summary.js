1a39bf6c3c5ef85d548e5956ae2d3dba6fb284aa
AryanSaini <09aryan.lucky@gmail.com>
2024-07-20 13:32:19 +0530

    add datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,32 @@
+// Primitive datatypes
+
+// 7 types : string, number, boolean, null, undefined, symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+let id = Symbol("123");
+let anotherId = Symbol("123");
+
+// console.log(id === anotherId);
+
+const bigNumber = 12323423987478623989824982n; // bigint
+
+
+// Non - primitive datatypes
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+  name: "aryan",
+  age: 22,
+};
+
+
+let myFunction = function() {
+    console.log("hello world");
+}
