08b741b888f72359b213547c7f9ae1fdefbedea2
AryanSaini <09aryan.lucky@gmail.com>
2024-07-20 13:31:03 +0530

    add comparision

---
+++ 04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3; // not like this
+
+num1 = num2 = num3 = 2 + 2; // readability of code should be maintained
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // comparision operators will convert null into 0.
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
