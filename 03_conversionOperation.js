f2a5f08949f6264044cd7ecafc80f3d61efaf86b
AryanSaini <09aryan.lucky@gmail.com>
2024-07-20 13:29:29 +0530

    add conversion operations

---
+++ 03_conversionOperation.js
@@ -0,0 +1,48 @@
+let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score); // Number() wil convert into number
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "aryan";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn); // Boolean() will convert into boolean
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "aryan" => true
+
+let someNumber = 33;
+let somenumerInString = String(someNumber); // String() will convert into string
+// console.log(somenumerInString);
+// console.log(typeof somenumerInString);
+
+// *************************  Operations  *************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log("Aryan" + " Saini");
+// console.log("Aryan" +" "+ "Saini");
+
+let str1 = "aryan";
+let str2 = " saini";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);    // if first one is string then all of them will be treated as string
+// console.log(1 + 2 + "2");    // if last one is string then opertion will occur
+
+// console.log( (3 + 4) * 5 %3);
+
