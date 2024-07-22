96bc213591f48e2b4f5771dce3c742213e7ea37d
AryanSaini <09aryan.lucky@gmail.com>
2024-07-22 16:25:51 +0530

    add numbers

---
+++ 07_numsAndMaths.js
@@ -0,0 +1,21 @@
+const score = 400;
+
+const balance = new Number(100); // strictly saying this is a number
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 23.8966;
+// console.log(otherNumber.toPrecision(1));    // returns a string
+// console.log(otherNumber.toPrecision(2));
+// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(5));
+// console.log(otherNumber.toPrecision(6));
+
+const hundreds = 1000000;
+// console.log(hundreds);
+// console.log(hundreds.toLocaleString('en-IN'));  // The toLocaleString() method of Date instances returns a string with a language-sensitive representation of this date in the local timezone
+
