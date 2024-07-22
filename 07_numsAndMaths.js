6a6bdc0b61d657a58f170c3d5b3931ddc5c6a377
AryanSaini <09aryan.lucky@gmail.com>
2024-07-22 16:44:19 +0530

    add maths

--- 07_numsAndMaths.js
+++ 07_numsAndMaths.js
@@ -19,3 +19,21 @@ const hundreds = 1000000;
 // console.log(hundreds);
 // console.log(hundreds.toLocaleString('en-IN'));  // The toLocaleString() method of Date instances returns a string with a language-sensitive representation of this date in the local timezone
 
+// ++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4,3,0,2,1));
+// console.log(Math.max(4,2,0,5));
+
+// console.log(Math.random());               // gives a random integer b/w 0 & 1
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
