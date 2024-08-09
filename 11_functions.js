fbaa22ec5fffc3162890ceb2ba77f4b4e8aa4f34
AryanSaini <09aryan.lucky@gmail.com>
2024-08-09 22:52:57 +0530

    add more in functions

--- 11_functions.js
+++ 11_functions.js
@@ -39,4 +39,41 @@ function loginUserMessage(username) {
 }
 
 // console.log( loginUserMessage("aryan"));
-console.log(loginUserMessage()); // if we don't pass any argument then we will get "undefined"
+// console.log(loginUserMessage()); // if we don't pass any argument then we will get "undefined"
+
+
+// --------- more on functions -------------
+
+function calculateCartPrice(val1, val2, ...num1) {        // using rest in function parameter
+  // rest in function
+  return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, 600, 1000));
+
+const user = {                          // passing object as parameter
+  username: "aryan",
+  price: "199",
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+
+// handleObject(user)            
+// handleObject({
+//     username: "Sam",
+//     price: 399
+// })
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondArray(getArray) {         // passing array as parameter
+  return getArray[1];
+}
+
+// console.log(returnSecondArray(myNewArray));
+console.log(returnSecondArray([300, 500, 800, 900]));
+
