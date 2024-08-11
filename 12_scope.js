1ca5ef3bb555213cff13aff8eb52f7192ba03f3c
AryanSaini <09aryan.lucky@gmail.com>
2024-08-11 15:17:08 +0530

    add more in scope

--- 12_scope.js
+++ 12_scope.js
@@ -15,6 +15,46 @@ if (true) {
   var c = 30;
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+// -------------- nested scope--------------
+
+function one() {
+  const username = "hitesh";
+
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  // console.log(website);
+  two();
+}
+
+// one();
+
+if (true) {
+  const username = "aryan";
+  if (username === "aryan") {
+    const website = " youtube";
+    // console.log(username + website);
+  }
+  // console.log(website);
+}
+
+// console.log(username);      // will returns an error because username is defined inside the scope of if.
+
+// ++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++
+
+addOne();                            // this will work too
+function addOne(num) {
+  return num + 1;
+}
+
+const addTwo = function (num) {
+  // Function expression
+  return num + 2;
+};
+
+addTwo(2);
