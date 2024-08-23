77cdc4408fef01822985a52279c940f6a67db6ad
AryanSaini <09aryan.lucky@gmail.com>
2024-08-23 16:10:32 +0530

    add break and continue

--- 16_loops.js
+++ 16_loops.js
@@ -8,17 +8,37 @@ for (let i = 0; i <= 10; i++) {
   //   console.log(element);
 }
 
-for (let i = 0; i <= 10; i++) {                             // nesting of for loop
+for (let i = 0; i <= 10; i++) {                            // nesting of for loop
   //   console.log(`Outer loop value ${i}`);
   for (let j = 0; j <= 5; j++) {
     // console.log(`Inner loop value ${j} and outer loop ${i}`);
   }
 }
+// Inner loop will execute only when condition of outer loop comes out to be true
 
 let myArray = ["falsh", "batman", "superman"];
 // console.log(myArray.length);
 for (let index = 0; index < myArray.length; index++) {
   const element = myArray[index];
-//   console.log(element);
+  //   console.log(element);
 }
 
+// break and continue
+
+// for (let index = 0; index <= 20; index++) {
+//   const element = index;
+//   if (index == 5) {
+//     console.log(`5 detected`);
+//     break                               // The break statement "jumps out" of the loop.
+//   }
+//   console.log(index);
+// }
+
+for (let index = 0; index <= 20; index++) {
+  const element = index;
+  if (index == 5) {
+    console.log(`5 detected`);
+    continue;                                   // The continue statement "jumps over" one iteration in the loop.
+  }
+  console.log(`Value of index is ${index}`);
+}
